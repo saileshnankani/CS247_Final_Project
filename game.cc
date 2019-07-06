ee59a6fa005c7643b6865189a92a1834e5dacff8
saileshnankani <saileshhps@gmail.com>
2019-07-06 17:13:11 -0400

    can now read input maps

--- game.cc
+++ game.cc
@@ -11,15 +11,37 @@ using namespace std;
 Game::Game() : gameOver{false} {
     string line;
 
-    ifstream mcFile ("/locations/mc.in");
+    ifstream mcFile;
+    ifstream tathamFile;
+    ifstream californiaFile;
+
+    mcFile.open("locations/mc.in");
+    tathamFile.open("locations/tatham.in");
+    californiaFile.open("locations/california.in");
 
     if(mcFile.is_open()){
-        cout<<"Sailesh"<<endl;
         while(getline(mcFile,line)){
             cout<<line<<endl;
-        }
+        }   
         mcFile.close();
     }
+    cout<<endl;
+
+    if(tathamFile.is_open()){
+        while(getline(tathamFile,line)){
+            cout<<line<<endl;
+        }   
+        tathamFile.close();
+    }
+    cout<<endl;
+
+    if(californiaFile.is_open()){
+        while(getline(californiaFile,line)){
+            cout<<line<<endl;
+        }   
+        californiaFile.close();
+    }
+    cout<<endl;
 }
 
 Game::~Game(){}
